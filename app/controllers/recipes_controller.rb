e0ca298d8dfb19eb737c952bbc1333beec3940b1
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-11-17 20:15:21 +0900

    投稿が保存できるように実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,8 +3,21 @@ class RecipesController < ApplicationController
   end
 
   def new
+    @recipe = Recipe.new
   end
 
   def create
+    @recipe =Recipe.new(recipe_params)
+    if @recipe.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:cuisine, :description, :foodstuff, :image).merge(user_id: current_user.id)
   end
 end
