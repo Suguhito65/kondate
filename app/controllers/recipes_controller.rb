66092ea8ef69cca465a5eeab301f60ca0861d965
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-11-19 16:20:27 +0900

    set_recipeでコードをまとめる

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,10 +1,10 @@
 class RecipesController < ApplicationController
+  before_action :set_recipe, only: [:show, :edit, :update]
   def index
     @recipes = Recipe.includes(:user)
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
   end
 
   def new
@@ -21,15 +21,23 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def update
+    if @recipe.update(recipe_params)
+      redirect_to recipe_path(@recipe.id)
+    else
+      render :edit
+    end
   end
 
   private
 
   def recipe_params
     params.require(:recipe).permit(:cuisine, :description, :foodstuff, :image).merge(user_id: current_user.id)
+  end
+
+  def set_recipe
+    @recipe = Recipe.find(params[:id])
   end
 end
