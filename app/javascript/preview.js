ffe683b68a1a5638ba773d7d207973c3e26fcd23
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-11-24 17:51:05 +0900

    プレビュー機能を実装

---
+++ app/javascript/preview.js
@@ -0,0 +1,22 @@
+if (document.URL.match( /new/ ) || document.URL.match( /edit/ )) {
+  document.addEventListener('DOMContentLoaded', function(){
+    const ImageList = document.getElementById('image-list');
+    document.getElementById('recipe_image').addEventListener('change', function(e){
+      const imageContent = document.querySelector('img');
+      if (imageContent) {
+        imageContent.remove();
+      }
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      const imageElement = document.createElement('div');
+
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+      blobImage.id = "preview";
+
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    });
+  });
+}
