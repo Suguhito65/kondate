557bfb5886919630e6275032c0ae832ba26794af
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-11-25 16:24:06 +0900

    showアクションに追加と遷移など

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "recipes#index"
-  resources :user, only: :show
+  resources :users, only: :show
   resources :recipes, except: :index do
     collection do
       get 'search'
