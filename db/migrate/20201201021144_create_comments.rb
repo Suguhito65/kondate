6e30c9e1ca0b0f2182e53ec6c73de857dbca31f3
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-12-01 11:31:48 +0900

    commentsテーブルを作成

--- db/migrate/20201201021144_create_comments.rb
+++ db/migrate/20201201021144_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
-      
+      t.text    :text
+      t.integer :user_id
+      t.integer :recipe_id
       t.timestamps
     end
   end
