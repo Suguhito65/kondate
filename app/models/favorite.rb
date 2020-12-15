9ef39dbddbb268f1fd460e6f9df2675e2b4d0e71
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-12-15 16:40:36 +0900

    アソシエーションを記述

--- app/models/favorite.rb
+++ app/models/favorite.rb
@@ -1,2 +1,6 @@
 class Favorite < ApplicationRecord
+  belongs_to :user
+  belongs_to :recipe
+
+  validates_uniqueness_of :recipe_id, scope: :user_id
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :recipes
+  has_many :favorites, dependent: :destroy
   has_many :comments
   
   validates :name, presence: true
