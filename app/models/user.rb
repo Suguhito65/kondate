a0c8035ccc41616b5cb4153dc621a25bf31470f9
すぐひと <69892326+Suguhito65@users.noreply.github.com>
2020-12-15 16:55:10 +0900

    いいね可否判定メソッドを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   has_many :recipes
   has_many :favorites, dependent: :destroy
   has_many :comments
   
   validates :name, presence: true
+
+  def already_favorited?(recipe)
+    self.favorites.exists?(recipe_id: recipe.id)
+  end
 end
